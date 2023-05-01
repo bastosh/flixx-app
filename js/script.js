b289287c18938051178d71f9d5d066fb2c7bf3a2
Sébastien Pereda <sebastien.pereda@gmail.com>
2023-05-01 21:06:30 +0200

    Fetch popular movies

--- js/script.js
+++ js/script.js
@@ -1,10 +1,50 @@
+const API_KEY = import.meta.env.VITE_API_KEY
+const API_URL = import.meta.env.VITE_API_URL
+
 const global = {
     currentPage: window.location.pathname
 }
 
+async function fetchApiData(enpoint) {
+    const response = await fetch(`${API_URL}/${enpoint}?api_key=${API_KEY}&language=fr-FR`)
+    return await response.json()
+}
+
+async function displayPopularMovies() {
+    const { results } = await fetchApiData('movie/popular')
+    results.forEach(movie => {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+            ${
+                movie.poster_path ? 
+                `<img
+                    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                    class="card-img-top"
+                    alt="${movie.title}"
+                />` 
+                :
+                `<img
+                    src="images/no-image.jpg"
+                    class="card-img-top"
+                    alt="No image"
+                />`
+            }
+        </a>
+        <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+                <small class="text-muted">Date de sortie : ${movie.release_date}</small>
+            </p>
+        </div>
+        `
+        document.getElementById('popular-movies').appendChild(div)
+    })
+}
+
 function highlightActiveLink() {
-    const links = document.querySelectorAll('.nav-link')
-    links.forEach(link => {
+    document.querySelectorAll('.nav-link').forEach(link => {
         if (link.getAttribute('href') === global.currentPage) {
             link.classList.add('active')
         }
@@ -15,7 +55,7 @@ function initApp() {
     switch(global.currentPage) {
         case '/': 
         case '/index.html': 
-            console.log('Home')
+            displayPopularMovies()
             break
         case '/shows.html':
             console.log('Shows')
