b5a831a329bb6211773b89679324b8794e83f829
Sébastien Pereda <sebastien.pereda@gmail.com>
2023-05-01 22:22:47 +0200

    Fetch movie details

--- js/script.js
+++ js/script.js
@@ -46,6 +46,59 @@ async function displayPopularMovies() {
     })
 }
 
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1]
+    const movie = await fetchApiData(`movie/${movieId}`)
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <div class="details-top">
+        <div>
+            ${
+                movie.poster_path ? 
+                `<img
+                    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                    class="card-img-top"
+                    alt="${movie.title}"
+                />` 
+                :
+                `<img
+                    src="images/no-image.jpg"
+                    class="card-img-top"
+                    alt="Pas d’image"
+                />`
+            }
+        </div>
+        <div>
+            <h2>${movie.title}</h2>
+            <p>
+                <i class="fas fa-star text-primary"></i>
+                ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Date de sortie : ${movie.release_date}</p>
+            <p>${movie.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+                ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visiter la page web du film</a>
+        </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Infos</h2>
+    <ul>
+        <li><span class="text-secondary">Budget :</span> ${formatNumber(movie.budget)} $</li>
+        <li><span class="text-secondary">Revenus :</span> ${formatNumber(movie.revenue)} $</li>
+        <li><span class="text-secondary">Durée :</span> ${movie.runtime} minutes</li>
+    </ul>
+    <h4>Compagnies de production</h4>
+    <div class="list-group">
+        ${movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}
+    </div>
+  </div>
+    `
+    document.getElementById('movie-details').appendChild(div)
+}
+
 async function displayPopularShows() {
     const { results } = await fetchApiData('tv/popular')
     results.forEach(show => {
@@ -79,6 +132,9 @@ async function displayPopularShows() {
     })
 }
 
+function formatNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+}
 
 function highlightActiveLink() {
     document.querySelectorAll('.nav-link').forEach(link => {
@@ -106,7 +162,7 @@ function initApp() {
             displayPopularShows()
             break
         case '/movie-details.html':
-            console.log('Movie details')
+            displayMovieDetails()
             break
         case '/tv-details.html':
             console.log('TV details')
