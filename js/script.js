dd5838822e8f65d1ae0596963854f0c273751fea
Sébastien Pereda <sebastien.pereda@gmail.com>
2023-05-02 09:27:49 +0200

    Add slider

--- js/script.js
+++ js/script.js
@@ -211,6 +211,52 @@ async function displayPopularShows() {
     })
 }
 
+async function displaySlider() {
+    const { results } = await fetchApiData('movie/now_playing')
+  
+    results.forEach((movie) => {
+      const div = document.createElement('div')
+      div.classList.add('swiper-slide')
+  
+      div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+        </a>
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+        </h4>
+      `
+  
+      document.querySelector('.swiper-wrapper').appendChild(div)
+  
+      initSwiper()
+    })
+  }
+  
+  function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+        },
+        700: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    })
+  }
+
 function formatNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 }
@@ -235,6 +281,7 @@ function initApp() {
     switch(global.currentPage) {
         case '/': 
         case '/index.html': 
+            displaySlider()
             displayPopularMovies()
             break
         case '/shows.html':
